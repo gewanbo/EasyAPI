3f49a2335d0b15dd90b88679ced97c30ab38db79
wanbo <gewanbo@gmail.com>
2015-04-09 14:57:23 +0800

    Add some fruits elements.

--- server/src/main/scala/com/wanbo/easyapi/server/lib/SeederManager.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/lib/SeederManager.scala
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.{JSONException, JSONObject, JSON}
 import com.wanbo.easyapi.server.workers.Seeder_10001
 import org.slf4j.LoggerFactory
 
-
 /**
  * The manager of all types workers
  * Created by GWB on 2015/4/8.
@@ -14,6 +13,7 @@ class SeederManager(seed: String) {
     private val log = LoggerFactory.getLogger(classOf[SeederManager])
 
     private var messageid: String = _
+    private var transactionType: String = _
 
     private var _seed: JSONObject = _
     private var _fruit: JSONObject = _
@@ -27,16 +27,21 @@ class SeederManager(seed: String) {
             if(messageid == null)
                 messageid = "139287742832"
 
-            _fruit = new JSONObject()
-            val head = new JSONObject()
-            head.put("messageid", messageid)
-            _fruit.put("head", head)
+            transactionType = _seed.getString("transactiontype")
+
+//            if(transactionType == null || transactionType == "")
+//                throw new Exception("Can't find the TransactionType!")
+
+
+
+
+
         } catch {
             case je: JSONException =>
                 log.warn("The seed string is :" + _seed)
                 log.error("Throws exception when parse seed:", je)
-            case _ =>
-                log.warn("The seed string is :" + _seed)
+            case e: Exception =>
+                log.error("The seed string is :" + _seed, e)
         }
     }
 
@@ -46,7 +51,22 @@ class SeederManager(seed: String) {
 
         val fruits = Seeder_10001.apply().onHandle(_seed)
 
-        _fruit.put("body", fruits)
+
+        _fruit = new JSONObject()
+        val head = new JSONObject()
+        head.put("messageid", messageid)
+        head.put("transactiontype", transactionType)
+
+        val body = new JSONObject()
+        val oelement = new JSONObject()
+        oelement.put("errorcode", 0)
+        oelement.put("errormsg", "")
+        body.put("oelement", oelement)
+        body.put("odata", {fruits})
+
+
+        _fruit.put("head", head)
+        _fruit.put("body", body)
 
         _fruit.toJSONString
     }
