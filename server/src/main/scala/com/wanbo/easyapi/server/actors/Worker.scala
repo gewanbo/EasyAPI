f64c1fc0d1630daf6f2572b0c73c5426fc275cdd
wanbo <gewanbo@gmail.com>
2015-04-10 10:38:34 +0800

    Change to Akka IO/Tcp

--- server/src/main/scala/com/wanbo/easyapi/server/actors/Worker.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/actors/Worker.scala
@@ -1,9 +1,8 @@
 package com.wanbo.easyapi.server.actors
 
-import java.io.{PrintWriter, InputStreamReader, BufferedReader}
-import java.net.Socket
-
 import akka.actor.Actor
+import akka.io.Tcp.{Close, PeerClosed, Write, Received}
+import akka.util.ByteString
 import com.wanbo.easyapi.server.lib.SeederManager
 import com.wanbo.easyapi.server.messages.Seed
 import org.slf4j.LoggerFactory
@@ -19,63 +18,50 @@ class Worker extends Actor {
     override def receive: Receive = {
         case Seed(client) =>
             log.info("I'm working ...")
-            val ret = process(client)
 
+        case Received(data) =>
+            var msgBody = ""
+
+            val msgData = data.decodeString("UTF-8")
+            msgData.split("\r").foreach(x => {
+                val body = x.trim
+                if(body.startsWith("{") && body.endsWith("}"))
+                    msgBody = body
+            })
+
+            val fruits = process(msgBody)
+
+            sender() ! Write(ByteString.fromString(fruits, "UTF-8"))
+            sender() ! Close
+        case PeerClosed =>
+            context stop self
     }
 
-    def process(client: Socket): String = {
+    def process(message: String): String = {
 
-        var message = ""
+        var fruits = ""
 
         try {
 
             log.info("Receive a message:")
 
-            // In message
-            val in = new BufferedReader(new InputStreamReader(client.getInputStream))
-
-            // Out message
-            val out = new PrintWriter(client.getOutputStream, true)
-
-            var fruits = ""
-
-            try {
-                var msgLength = 10
-                //val headers = in.readLine()
-                message = in.readLine().trim
-
-                while (msgLength > 0 && message != null && !message.startsWith("{") && !message.endsWith("}")) {
-                    msgLength -= 1
-                    println(message)
-                    message = in.readLine().trim
-                }
-
-                if (message == "") {
-                    throw new Exception("Request body is empty!")
-                }
-
-                val seederManager = new SeederManager(message)
-
-                fruits = seederManager.farming()
-
-            } catch {
-                case e: Exception =>
-                    fruits = """{"errorcode": 99999, "errormsg": %s}""".format(e.getMessage)
+            if (message == "") {
+                throw new Exception("Request body is empty!")
             }
-            // Response message
-            out.println(fruits)
 
-            log.info("Input message is:" + message)
-            log.info("Output message is:" + fruits)
+            val seederManager = new SeederManager(message)
 
-            out.close()
-            in.close()
-            client.close()
+            fruits = seederManager.farming()
+
         } catch {
             case e: Exception =>
+                fruits = """{"errorcode": 99999, "errormsg": %s}""".format(e.getMessage)
                 log.error("Message worker process exception :", e)
         }
 
-        message
+        log.info("Input message is:" + message)
+        log.info("Output message is:" + fruits)
+
+        fruits
     }
 }
