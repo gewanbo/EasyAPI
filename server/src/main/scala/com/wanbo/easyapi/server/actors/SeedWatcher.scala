77954456537d0ba91570c4d275fce1d6cc815c66
wanbo <gewanbo@gmail.com>
2015-04-10 10:42:27 +0800

    Change to Akka IO

--- server/src/main/scala/com/wanbo/easyapi/server/actors/SeedWatcher.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/actors/SeedWatcher.scala
@@ -1,8 +1,10 @@
 package com.wanbo.easyapi.server.actors
 
-import java.net.ServerSocket
+import java.net.{InetSocketAddress, ServerSocket}
 
 import akka.actor.{Props, Actor}
+import akka.io.Tcp.{Write, Register, Connected, Bind}
+import akka.io.{Tcp, IO}
 import akka.routing.{DefaultResizer, RoundRobinRouter}
 import com.wanbo.easyapi.server.lib.EasyConfig
 import com.wanbo.easyapi.server.messages._
@@ -12,60 +14,25 @@ import com.wanbo.easyapi.server.messages._
  *
  * Created by wanbo on 15/4/3.
  */
-class SeedWatcher(conf: EasyConfig) extends Actor {
+class SeedWatcher(conf: EasyConfig, port: Int) extends Actor {
 
-    var socket: ServerSocket = _
-    var isClose: Boolean = false
+    import context.system
 
     val resizer = DefaultResizer(lowerBound=1, upperBound = conf.workersMaxThreads)
 
     val worker = context.actorOf(Props[Worker].withRouter(RoundRobinRouter(resizer = Some(resizer))), name = "worker")
 
+    IO(Tcp) ! Bind(self, new InetSocketAddress(conf.serverHost, port))
+
     override def receive: Receive = {
 
-        case ListenerWorkerStart(port: Int) =>
+        case c @ Connected(remoteAddress, localAddress) =>
+            sender() ! Register(worker)
 
-            // Workers' watcher
-            val stat = workerListen(port)
-
-            if(!stat) {
-                sender() ! ListenerFailed
-            } else {
-                self ! ListenerWorkProcess(port)
-            }
-
-        case ListenerWorkProcess(port) =>
-            if(!isClose) {
-                workerListen(port)
-                self ! ListenerWorkProcess(port)
-            }
 
         case ListenerWorkerStop =>
-            isClose = true
-            socket.close()
             context.stop(worker)
             context.stop(self)
     }
 
-    def workerListen(port: Int): Boolean ={
-        var ret = true
-
-        try {
-
-            if(!isClose && socket == null) {
-                socket = new ServerSocket(port)
-            }
-
-            val client = socket.accept()
-            if (client != null) {
-                worker ! Seed(client)
-            }
-        } catch {
-            case e: Exception =>
-                println(e.getMessage)
-                ret = false
-        }
-
-        ret
-    }
 }
