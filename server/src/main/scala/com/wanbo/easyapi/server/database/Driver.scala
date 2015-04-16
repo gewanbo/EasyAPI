76573a2f2df52b0443765940c4301bce85194a8d
wanbo <gewanbo@gmail.com>
2015-04-16 19:06:05 +0800

    The database driver traits.

--- server/src/main/scala/com/wanbo/easyapi/server/database/Driver.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/database/Driver.scala
@@ -1,9 +1,11 @@
 package com.wanbo.easyapi.server.database
 
+import com.wanbo.easyapi.server.lib.EasyConfig
+
 /**
  * Database driver
  * Created by wanbo on 15/4/16.
  */
 trait Driver {
-
+    def setConfiguration(conf: EasyConfig)
 }
