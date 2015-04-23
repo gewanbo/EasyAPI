0c8786f16a1f25cd1891a7c3aadee6d78e3fbcbb
wanbo <gewanbo@gmail.com>
2015-04-23 17:20:41 +0800

    Add HBase driver class.

---
+++ server/src/main/scala/com/wanbo/easyapi/server/database/HBaseDriver.scala
@@ -0,0 +1,13 @@
+package com.wanbo.easyapi.server.database
+
+import com.wanbo.easyapi.server.lib.EasyConfig
+
+/**
+ * HBase driver.
+ * Created by wanbo on 15/4/17.
+ */
+case class HBaseDriver() extends Driver {
+    override def setConfiguration(conf: EasyConfig): Unit = {
+
+    }
+}
