48fc5305c23281145b74340a2682880716f2b36c
wanbo <gewanbo@gmail.com>
2015-04-27 16:17:52 +0800

    Dynamic get configuration parameters.

--- server/src/main/scala/com/wanbo/easyapi/server/lib/EasyConfig.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/lib/EasyConfig.scala
@@ -1,5 +1,10 @@
 package com.wanbo.easyapi.server.lib
 
+import java.io.FileInputStream
+import java.util.Properties
+
+import org.slf4j.LoggerFactory
+
 /**
  * The configuration of EasyApi
  * Created by wanbo on 15/4/8.
@@ -15,4 +20,26 @@ class EasyConfig() {
     var workersMaxThreads: Int = _
 
     var driver_mysql = Map[String, String]()
+
+    private val log = LoggerFactory.getLogger(classOf[EasyConfig])
+
+    def getConfigure(key: String): String ={
+        var value = ""
+
+        try {
+
+            val confProps = new Properties()
+            val configFile = System.getProperty("easy.conf", "config.properties")
+            confProps.load(new FileInputStream(configFile))
+
+            if(confProps.containsKey(key))
+                value = confProps.getProperty(key, "")
+
+        } catch {
+            case e: Exception =>
+                log.error("There throws exception when load configure file:", e)
+        }
+
+        value
+    }
 }
