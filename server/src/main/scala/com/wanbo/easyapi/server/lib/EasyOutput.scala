c5ae1905aa24f86e09862012fd81a23f7d9a4386
wanbo <gewanbo@gmail.com>
2015-04-29 16:04:07 +0800

    Change the output data type.

--- server/src/main/scala/com/wanbo/easyapi/server/lib/EasyOutput.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/lib/EasyOutput.scala
@@ -6,5 +6,5 @@ package com.wanbo.easyapi.server.lib
  */
 class EasyOutput extends Serializable {
     var oelement: Map[String, String] = Map(("errorcode", "-1"), ("errormsg", ""))
-    var odata: List[List[(String, Any)]] = _
+    var odata: List[Map[String, Any]] = _
 }
