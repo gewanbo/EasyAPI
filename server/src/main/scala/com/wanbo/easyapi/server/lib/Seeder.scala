5ed85cca5c163d93429d6be1d9c44ca77f9b3cf3
wanbo <gewanbo@gmail.com>
2015-04-30 09:37:38 +0800

    Change the fruits output type.

--- server/src/main/scala/com/wanbo/easyapi/server/lib/Seeder.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/lib/Seeder.scala
@@ -1,7 +1,5 @@
 package com.wanbo.easyapi.server.lib
 
-import com.alibaba.fastjson.JSONObject
-
 /**
  * The abstract class of seeders
  * Created by GWB on 2015/4/8.
@@ -10,7 +8,7 @@ abstract class Seeder {
 
     var name: String = _
 
-    protected var fruits: JSONObject = new JSONObject()
+    protected var fruits: EasyOutput = new EasyOutput()
 
     protected def onDBHandle(): Any
 }
