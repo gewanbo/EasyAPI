ce062438a642285428f2c04b320ca3cec80877f0
wanbo <gewanbo@gmail.com>
2015-04-30 09:39:43 +0800

    Number 11001 seeder.

---
+++ server/src/main/scala/com/wanbo/easyapi/server/workers/Seeder_11001.scala
@@ -0,0 +1,101 @@
+package com.wanbo.easyapi.server.workers
+
+import java.util.{Calendar, TimeZone}
+
+import com.alibaba.fastjson.{JSONArray, JSONObject}
+import com.wanbo.easyapi.server.database.MysqlDriver
+import com.wanbo.easyapi.server.lib.{EasyOutput, EasyException, ISeeder, Seeder}
+import org.slf4j.LoggerFactory
+
+
+/**
+ * Number 11001 seeder
+ * Created by wanbo on 28/4/16.
+ */
+final class Seeder_11001 extends Seeder with ISeeder {
+
+    name = "11001"
+
+    driver = new MysqlDriver
+
+    private val log = LoggerFactory.getLogger(classOf[Seeder_11001])
+
+    override def onHandle(seed: Map[String, Any]): EasyOutput = {
+
+        try {
+
+            val data = onDBHandle()
+
+            if(data.size < 1)
+                throw new EasyException("20100")
+            else
+                fruits.odata = list2tree("0", data)
+
+            fruits.oelement = fruits.oelement.updated("errorcode", "0")
+        } catch {
+            case ee: EasyException =>
+                fruits.oelement = fruits.oelement.updated("errorcode", ee.getCode)
+
+            case e: Exception =>
+                log.error("Seeder has exception:", e)
+                fruits.oelement = fruits.oelement.updated("errorcode", "-1")
+        }
+
+        fruits
+    }
+    override protected def onDBHandle(): List[(String, String, String, String, String, String, String)] = {
+        var dataList = List[(String, String, String, String, String, String, String)]()
+
+        try {
+            val driver = this.driver.asInstanceOf[MysqlDriver]
+            driver.setDB("cmstmp01")
+            val conn = driver.getConnector
+
+            val sql = "SELECT id,reid,`code`,`name`,`type`,`link`,haschildren FROM `channel`  ORDER BY `reid` DESC,`id`"
+
+            val ps = conn.prepareStatement(sql)
+            val rs = ps.executeQuery()
+
+            while (rs.next()){
+                dataList = dataList :+ (rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7))
+            }
+
+            rs.close()
+            ps.close()
+
+        } catch {
+            case e: Exception =>
+                throw e
+        }
+
+        dataList
+    }
+
+    private def list2tree(reId: String, list: List[(String, String, String, String, String, String, String)]): List[Map[String, Any]] ={
+        var jList = List[Map[String, Any]]()
+        val aList = list.filter(_._2 == reId)
+        val bList = list.diff(aList)
+
+        if(bList.size > 0) {
+            aList.foreach(x => {
+                var tmpMap = Map[String, Any]()
+
+                tmpMap = tmpMap + ("id" -> x._1)
+                tmpMap = tmpMap + ("reid" -> x._2)
+                tmpMap = tmpMap + ("code" -> x._3)
+                tmpMap = tmpMap + ("name" -> x._4)
+                tmpMap = tmpMap + ("type" -> x._5)
+
+                if(x._5 != "link" && x._3 != ""){
+                    tmpMap = tmpMap + ("link" -> "/%s/%s.html".format(x._5, x._3))
+                } else {
+                    tmpMap = tmpMap + ("link" -> x._6)
+                }
+                tmpMap = tmpMap + ("haschildren" -> x._7)
+                tmpMap = tmpMap + ("children" -> list2tree(x._1, bList))
+                jList = jList :+ tmpMap
+            })
+        }
+        jList
+    }
+}
