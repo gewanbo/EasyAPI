8d1ccd5d2f5afb1507105ec9ff97e7d8a5bbcf11
wanbo <gewanbo@gmail.com>
2015-05-14 12:10:03 +0800

    Add Tcp bind state messages.

--- server/src/main/scala/com/wanbo/easyapi/server/actors/SeedWatcher.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/actors/SeedWatcher.scala
@@ -3,7 +3,7 @@ package com.wanbo.easyapi.server.actors
 import java.net.{InetSocketAddress, ServerSocket}
 
 import akka.actor.{Props, Actor}
-import akka.io.Tcp.{Write, Register, Connected, Bind}
+import akka.io.Tcp._
 import akka.io.{Tcp, IO}
 import akka.routing.{DefaultResizer, RoundRobinRouter}
 import com.wanbo.easyapi.server.lib.EasyConfig
@@ -25,6 +25,13 @@ class SeedWatcher(conf: EasyConfig, port: Int) extends Actor {
     IO(Tcp) ! Bind(self, new InetSocketAddress(conf.serverHost, port))
 
     override def receive: Receive = {
+
+        case b @ Bound(localAddress) =>
+            // Bound success
+
+        case CommandFailed(_: Bind) =>
+            // Close listeners and then
+            sender() ! ListenerFailed
 
         case c @ Connected(remoteAddress, localAddress) =>
             sender() ! Register(worker)
