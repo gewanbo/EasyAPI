eca0f1e50b97ed3bee5a1e794e5a54292c445778
wanbo <gewanbo@gmail.com>
2015-07-07 10:49:46 +0800

    The parent class of zk clients.

---
+++ server/src/main/scala/com/wanbo/easyapi/server/lib/ZookeeperManager.scala
@@ -0,0 +1,14 @@
+package com.wanbo.easyapi.server.lib
+
+/**
+ * The manager of Zookeeper.
+ * Created by wanbo on 15/7/7.
+ */
+class ZookeeperManager {
+
+    protected val app_root = "/easyapi"
+
+    protected val server_root = "/servers"
+    protected val client_root = "/clients"
+
+}
