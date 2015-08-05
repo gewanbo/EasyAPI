5c52327ba386411a610f42cbca0709599f87ebe7
wanbo <gewanbo@gmail.com>
2015-08-05 14:30:42 +0800

    Normal configure when initialize driver.

--- server/src/main/scala/com/wanbo/easyapi/server/database/HBaseDriver.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/database/HBaseDriver.scala
@@ -1,13 +1,24 @@
 package com.wanbo.easyapi.server.database
 
 import com.wanbo.easyapi.server.lib.EasyConfig
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.HBaseConfiguration
 
 /**
  * HBase driver.
  * Created by wanbo on 15/4/17.
  */
 case class HBaseDriver() extends Driver {
+    private var db_zq: String = _
+
     override def setConfiguration(conf: EasyConfig): Unit = {
 
+        db_zq = conf.getConfigure("hbase.zookeeper.quorum")
+    }
+
+    def getHConf: Configuration ={
+        val hConf = HBaseConfiguration.create()
+        hConf.set("hbase.zookeeper.quorum", db_zq)
+        hConf
     }
 }
