2e93d9e9d3ec84f8f2fb7ce6f73f6d5a1b551f22
wanbo <gewanbo@gmail.com>
2015-08-05 14:41:35 +0800

    Normal configure when initialize driver.

--- server/src/main/scala/com/wanbo/easyapi/server/database/HBaseDriver.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/database/HBaseDriver.scala
@@ -3,13 +3,16 @@ package com.wanbo.easyapi.server.database
 import com.wanbo.easyapi.server.lib.EasyConfig
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.hbase.HBaseConfiguration
+import org.slf4j.LoggerFactory
 
 /**
  * HBase driver.
  * Created by wanbo on 15/4/17.
  */
 case class HBaseDriver() extends Driver {
-    private var db_zq: String = _
+    private var db_zq: String = ""
+
+    private val log = LoggerFactory.getLogger(classOf[HBaseDriver])
 
     override def setConfiguration(conf: EasyConfig): Unit = {
 
@@ -18,7 +21,18 @@ case class HBaseDriver() extends Driver {
 
     def getHConf: Configuration ={
         val hConf = HBaseConfiguration.create()
-        hConf.set("hbase.zookeeper.quorum", db_zq)
+
+        try {
+
+            if(db_zq == "")
+                throw new Exception("The configuration option [hbase.zookeeper.quorum] is empty!")
+
+            hConf.set("hbase.zookeeper.quorum", db_zq)
+        } catch {
+            case e: Exception =>
+                log.error("HBase configure exception:", e)
+        }
+
         hConf
     }
 }
