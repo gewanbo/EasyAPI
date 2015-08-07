b3e4477452068e09cdde793483357595b49d0551
wanbo <gewanbo@gmail.com>
2015-08-07 10:52:32 +0800

    Close cache manager.

--- server/src/main/scala/com/wanbo/easyapi/server/workers/Seeder_10006.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/workers/Seeder_10006.scala
@@ -69,6 +69,7 @@ final class Seeder_10006 extends Seeder with ISeeder {
                     cacher.cacheData(cache_name, cache_data)
                 }
             }
+            cacher.close()
 
             fruits.oelement = fruits.oelement.updated("errorcode", "0")
             fruits.odata = dataList
