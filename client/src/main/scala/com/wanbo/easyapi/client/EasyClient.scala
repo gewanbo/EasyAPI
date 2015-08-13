0afebb829154456ea497459d513b36e9cef43f8a
wanbo <gewanbo@gmail.com>
2015-08-13 16:37:04 +0800

    The client of API.

---
+++ client/src/main/scala/com/wanbo/easyapi/client/EasyClient.scala
@@ -0,0 +1,12 @@
+package com.wanbo.easyapi.client
+
+/**
+ * EasyClient
+ * Created by wanbo on 15/8/13.
+ */
+object EasyClient {
+    def main(args: Array[String]) {
+
+        println("Easy client start up ...")
+    }
+}
