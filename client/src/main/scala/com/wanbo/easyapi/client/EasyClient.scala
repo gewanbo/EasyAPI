8eebd8e8386d85447fad260a9808be0ac86f5201
wanbo <gewanbo@gmail.com>
2015-08-14 17:55:51 +0800

    Add todo list for client.

--- client/src/main/scala/com/wanbo/easyapi/client/EasyClient.scala
+++ client/src/main/scala/com/wanbo/easyapi/client/EasyClient.scala
@@ -8,5 +8,16 @@ object EasyClient {
     def main(args: Array[String]) {
 
         println("Easy client start up ...")
+
+        // ### Todo list
+
+        // Initialize zk watcher
+
+        // Initialize server list
+
+        // Register client to zk
+
+        // Register socket service
+
     }
 }
