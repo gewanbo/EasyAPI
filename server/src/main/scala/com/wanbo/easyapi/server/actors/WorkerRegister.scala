df00ea29a144171c36e5f6e08f6d94c03c631fe4
wanbo <gewanbo@gmail.com>
2015-08-17 14:38:02 +0800

    Switch to shared lib.

--- server/src/main/scala/com/wanbo/easyapi/server/actors/WorkerRegister.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/actors/WorkerRegister.scala
@@ -1,8 +1,7 @@
 package com.wanbo.easyapi.server.actors
 
 import akka.actor.Actor
-import com.wanbo.easyapi.server.lib.EasyConfig
-import com.wanbo.easyapi.shared.common.libs.ZookeeperManager
+import com.wanbo.easyapi.shared.common.libs.{EasyConfig, ZookeeperManager}
 import com.wanbo.easyapi.shared.common.utils.ZookeeperClient
 import org.apache.zookeeper.CreateMode
 import org.slf4j.LoggerFactory
