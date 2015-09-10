82c7d83a1657bd54630fdd66fb1622e15b316418
wanbo <gewanbo@gmail.com>
2015-09-10 14:42:20 +0800

    add some exception error message.

--- server/src/main/scala/com/wanbo/easyapi/server/lib/ErrorConstant.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/lib/ErrorConstant.scala
@@ -16,6 +16,9 @@ object ErrorConstant {
     errorList = errorList.+("20010" -> "The length of UserName was wrong.")
     errorList = errorList.+("20100" -> "There is no data in results data set.")
 
+    errorList = errorList.+("40001" -> "The database connect failed.")
+    errorList = errorList.+("40002" -> "The database operation throws exception.")
+
     errorList = errorList.+("99999" -> "Undefined exception.")
 
     def getErrorMessage(code: String): String = {
