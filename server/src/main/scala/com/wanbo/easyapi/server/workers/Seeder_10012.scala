3db88ed7c4ac8fd9522d276ed69bf5c5cbfbc47c
wanbo <gewanbo@gmail.com>
2015-10-10 15:23:53 +0800

    Add new seeder.

---
+++ server/src/main/scala/com/wanbo/easyapi/server/workers/Seeder_10012.scala
@@ -0,0 +1,119 @@
+package com.wanbo.easyapi.server.workers
+
+import com.wanbo.easyapi.server.cache.CacheManager
+import com.wanbo.easyapi.server.database.MysqlDriver
+import com.wanbo.easyapi.server.lib.{EasyException, EasyOutput, ISeeder, Seeder}
+import org.slf4j.LoggerFactory
+
+
+/**
+ * Get photo news information by unique id.
+ * Created by wanbo on 2015/10/10.
+ */
+final class Seeder_10012 extends Seeder with ISeeder {
+
+    name = "10011"
+
+    driver = new MysqlDriver
+
+    private var _idSet = Set[String]()
+
+    private val log = LoggerFactory.getLogger(classOf[Seeder_10012])
+
+    override def onHandle(seed: Map[String, Any]): EasyOutput = {
+
+        var dataList = List[Map[String, Any]]()
+
+        try {
+            val idStr = seed.getOrElse("id", "").toString
+
+            val idArr = idStr.split(",")
+
+            if(idArr.size > 0) {
+                idArr.foreach(x => {
+                    if(x != "" && x.forall(_.isDigit))
+                        _idSet += x
+                })
+            } else {
+                throw new EasyException("20001")
+            }
+
+            if(_idSet.size < 1)
+                throw new EasyException("20001")
+
+            // Cache
+            val cache_name = this.getClass.getSimpleName + _idSet.hashCode()
+
+            val cacher = new CacheManager(conf = _conf, expire = 600)
+
+            val cacheData = cacher.cacheData(cache_name)
+
+            if (cacheData != null && cacheData.oelement.get("errorcode").get == "0" && !isUpdateCache) {
+                dataList = cacheData.odata
+                fruits.oelement = fruits.oelement + ("fromcache" -> "true") + ("ttl" -> cacher.ttl.toString)
+            } else {
+
+                dataList = onDBHandle()
+
+                if (dataList.size < 1)
+                    throw new EasyException("20100")
+                else {
+                    val cache_data = new EasyOutput
+                    cache_data.odata = dataList
+
+                    cache_data.oelement = cache_data.oelement.updated("errorcode", "0")
+                    cacher.cacheData(cache_name, cache_data)
+                }
+            }
+            cacher.close()
+
+            fruits.oelement = fruits.oelement.updated("errorcode", "0")
+            fruits.odata = dataList
+        } catch {
+            case ee: EasyException =>
+                fruits.oelement = fruits.oelement.updated("errorcode", ee.getCode)
+            case e: Exception =>
+                log.error("Seeder has exception:", e)
+                fruits.oelement = fruits.oelement.updated("errorcode", "-1")
+                fruits.oelement = fruits.oelement.updated("errormsg", e.getMessage)
+        }
+
+        fruits
+    }
+    override protected def onDBHandle(): List[Map[String, String]] = {
+        var dataList = List[Map[String, String]]()
+
+        try {
+            val driver = this.driver.asInstanceOf[MysqlDriver]
+            val conn = driver.getConnector("cmstmp01")
+
+            val sql = "SELECT `photonewsid` id,cn_title `title` FROM photonews where `photonewsid` in (%s)".format(_idSet.mkString("'", "','", "'"))
+
+            val ps = conn.prepareStatement(sql)
+            val rs = ps.executeQuery()
+
+            val metaData = ps.getMetaData
+            val columnCount = metaData.getColumnCount
+            while (rs.next()) {
+                var tmpMap = Map[String, String]()
+
+                for(i <- Range(1, columnCount + 1)) {
+                    tmpMap = tmpMap + (metaData.getColumnLabel(i) -> rs.getString(i))
+                }
+
+                dataList = dataList :+ tmpMap
+            }
+
+            rs.close()
+            ps.close()
+            conn.close()
+
+        } catch {
+            case e: Exception =>
+                throw e
+        }
+
+        dataList
+    }
+
+}
