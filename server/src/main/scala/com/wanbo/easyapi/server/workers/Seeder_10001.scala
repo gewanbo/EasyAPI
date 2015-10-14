4fb6c866e1f61ca2c22eb380988c27bde3d485c7
wanbo <gewanbo@gmail.com>
2015-10-14 17:16:17 +0800

    Add a demo seeder.

--- server/src/main/scala/com/wanbo/easyapi/server/workers/Seeder_10001.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/workers/Seeder_10001.scala
@@ -1,17 +1,19 @@
 package com.wanbo.easyapi.server.workers
 
-import com.alibaba.fastjson.JSONObject
-import com.wanbo.easyapi.server.lib.{Seeder, ISeeder}
+import com.wanbo.easyapi.server.lib.{EasyOutput, Seeder, ISeeder}
 
 /**
  * Number 10001 seeder
  * Created by GWB on 2015/4/8.
  */
-case class Seeder_10001() extends Seeder with ISeeder {
-    override def onHandle(seed: JSONObject): JSONObject = {
-        fruits = new JSONObject()
+final class Seeder_10001() extends Seeder with ISeeder {
 
-        fruits.put("messageid", "msg102039")
+    name = "10001"
+
+    override def onHandle(seed: Map[String, Any]): EasyOutput = {
+        fruits = new EasyOutput()
+
+        //fruits.put("messageid", "msg102039")
 
         fruits
     }
