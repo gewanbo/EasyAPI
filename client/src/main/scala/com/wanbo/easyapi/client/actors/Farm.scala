3fdd74eb0846ceedd59a6b25cf017587e3d6d389
wanbo <gewanbo@gmail.com>
2015-11-16 17:08:21 +0800

    Add some features.

--- client/src/main/scala/com/wanbo/easyapi/client/actors/Farm.scala
+++ client/src/main/scala/com/wanbo/easyapi/client/actors/Farm.scala
@@ -3,14 +3,14 @@ package com.wanbo.easyapi.client.actors
 import akka.actor.Actor
 import akka.io.Tcp.{Close, Write, PeerClosed, Received}
 import akka.util.ByteString
-import com.wanbo.easyapi.client.lib.AvailableServer
+import com.wanbo.easyapi.client.lib.{WorkCounter, AvailableServer}
 import org.slf4j.LoggerFactory
 
 import scala.util.Random
 
 /**
  * Farm
- * Created by wanbo on 15/8/17.
+ * Created by wanbo on 2015/8/17.
  */
 class Farm extends Actor {
 
@@ -21,34 +21,74 @@ class Farm extends Actor {
 
             log.info("Received a request ...")
 
-            // Get current server list
-            val servers = AvailableServer.serverList
+            var msgBody = data.decodeString("UTF-8")
 
-            // return the best one
-            var serverText = ""
+            val msgData = data.decodeString("UTF-8")
+            msgData.split("\r").foreach(x => {
+                val body = x.trim
+                if(body.startsWith("{") && body.endsWith("}"))
+                    msgBody = body
+            })
 
-            if(servers != null && servers.size > 0) {
-                if(servers.size > 2) {
-                    // Remove the biggest one, and random one form the rest.
-                    val biggest = servers.maxBy(_._2)
-                    val restServers = servers.filter(x => x != biggest)
-                    serverText = Farm.randomUniqueOneServer(restServers)
+            try {
+
+                if (msgBody.startsWith("{miss")) {
+
+                    var server = ""
+                    val fields = msgBody.substring(1, msgBody.size -1).split("#")
+
+                    if (fields.size > 1) {
+                        server = fields(1)
+
+                        AvailableServer.serverList.foreach(println)
+
+                        if (server != "" && AvailableServer.serverList.contains(server)) {
+                            // Get current server list
+                            WorkCounter.push(server)
+                        }
+                    }
+
+                    val responseBody = "HTTP/1.1 200 OK\nContent-Type: application/json\n" + "\n"
+
+                    sender() ! Write(ByteString.fromString(responseBody, "UTF-8"))
+                    sender() ! Close
                 } else {
-                    serverText = Farm.randomUniqueOneServer(servers)
+
+                    // Get current server list
+                    val servers = AvailableServer.serverList
+
+                    // return the best one
+                    var serverText = ""
+
+                    if (servers != null && servers.size > 0) {
+                        if (servers.size > 2) {
+                            // Remove the biggest one, and random one form the rest.
+                            val biggest = servers.maxBy(_._2)
+                            val restServers = servers.filter(x => x != biggest)
+                            serverText = Farm.randomUniqueOneServer(restServers)
+                        } else {
+                            serverText = Farm.randomUniqueOneServer(servers)
+                        }
+                    }
+
+                    if (serverText == "") {
+                        // Alarm
+                        log.error("Didn't find available server!")
+                    } else {
+                        log.info("The best server is:" + serverText)
+                    }
+
+                    val responseBody = "HTTP/1.1 200 OK\nContent-Type: application/json\n" + "\n" + serverText
+
+                    sender() ! Write(ByteString.fromString(responseBody, "UTF-8"))
+                    sender() ! Close
                 }
+            } catch {
+                case e: Exception =>
+                    log.error("Error:", e)
+                    sender() ! Write(ByteString.fromString("HTTP/1.1 200 OK\nContent-Type: application/json\n" + "\n", "UTF-8"))
+                    sender() ! Close
             }
-
-            if(serverText == ""){
-                // Alarm
-                log.error("Didn't find available server!")
-            } else {
-                log.info("The best server is:" + serverText)
-            }
-
-            val responseBody = "HTTP/1.1 200 OK\nContent-Type: application/json\n" + "\n" + serverText
-
-            sender() ! Write(ByteString.fromString(responseBody, "UTF-8"))
-            sender() ! Close
 
         case PeerClosed =>
             context stop self
