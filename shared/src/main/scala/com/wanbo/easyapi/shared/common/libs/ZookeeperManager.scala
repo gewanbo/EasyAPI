ed111d3389513147f99ead577cd4bc62b58f6d32
gewanbo <gewanbo@gmail.com>
2015-11-30 14:53:06 +0800

    Add znode initialization.

--- shared/src/main/scala/com/wanbo/easyapi/shared/common/libs/ZookeeperManager.scala
+++ shared/src/main/scala/com/wanbo/easyapi/shared/common/libs/ZookeeperManager.scala
@@ -1,14 +1,64 @@
 package com.wanbo.easyapi.shared.common.libs
 
+import com.wanbo.easyapi.shared.common.Logging
+import com.wanbo.easyapi.shared.common.utils.ZookeeperClient
+
 /**
  * The manager of Zookeeper.
  * Created by wanbo on 15/7/7.
  */
-class ZookeeperManager {
+class ZookeeperManager extends Logging {
 
     protected val app_root = "/easyapi"
 
     protected val server_root = "/servers"
     protected val client_root = "/clients"
 
+    protected val leader_root = "/leaders"
+
+    /**
+     * Initialize tree node structure.
+     * @param conf    Configuration
+     */
+    protected def initNodeTree(conf: EasyConfig): Unit ={
+
+        try {
+
+            log.info("Initialize the tree node in Zookeeper.")
+
+            val zk = new ZookeeperClient(conf.zkHosts, 3000, "/", Some(this.callback))
+
+            if(!zk.exists(app_root.replace("/", ""))){
+                zk.createPath(app_root.replace("/", ""))
+            }
+
+            val serversNode = (app_root + server_root).substring(1)
+
+            if(!zk.exists(serversNode)){
+                zk.createPath(serversNode)
+            }
+
+            val clientsNode = (app_root + client_root).substring(1)
+
+            if(!zk.exists(clientsNode)){
+                zk.createPath(clientsNode)
+            }
+
+            val leadersNode = (app_root + leader_root).substring(1)
+
+            if(!zk.exists(leadersNode)){
+                zk.createPath(leadersNode)
+            }
+
+            zk.close()
+
+            log.info("Tree node initialization successful.")
+
+        } catch {
+            case e: Exception =>
+                log.error("Error:", e)
+        }
+    }
+
+    private def callback(zk: ZookeeperClient): Unit ={}
 }
