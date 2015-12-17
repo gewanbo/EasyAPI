f922ed61ffd60af31a2ab342f6e27d709a1d9bb1
wanbo <gewanbo@gmail.com>
2015-12-17 14:15:19 +0800

    Add http utility functions.

--- client/src/main/scala/com/wanbo/easyapi/client/lib/HttpUtility.scala
+++ client/src/main/scala/com/wanbo/easyapi/client/lib/HttpUtility.scala
@@ -1,16 +1,66 @@
 package com.wanbo.easyapi.client.lib
 
+import java.io._
+import java.net.Socket
+
+import com.wanbo.easyapi.shared.common.Logging
+
 /**
  * Http utility
  * Created by root on 15-12-11.
  */
-object HttpUtility {
+object HttpUtility extends Logging {
 
     val jsonHeader = "HTTP/1.1 200 OK\nContent-Type: application/json\n\n"
     val textHeader = "HTTP/1.1 200 OK\nContent-Type: text/plain\n\n"
 
     def post(message: String): String ={
         var responseMsg = ""
+
+        try {
+
+            log.info("Post request to server with message:" + message)
+
+            val socket = new Socket("172.27.10.218", 8801)
+            socket.setSoTimeout(5000)
+
+            val outStream = socket.getOutputStream
+
+            val out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outStream)))
+
+            val inStream = new InputStreamReader(socket.getInputStream)
+            val in = new BufferedReader(inStream)
+
+            out.println(message)
+            out.flush()
+
+            val sb = new StringBuilder
+
+            var line = in.readLine()
+
+            while(line != null){
+                sb.++=(line + "\r")
+                line = in.readLine()
+            }
+
+            //log.info("Response message is1----:" + sb.toString())
+
+            responseMsg = parseBody(sb.toString())
+
+            //log.info("Response message is2----:" + responseMsg)
+
+            out.close()
+            outStream.close()
+
+            in.close()
+            inStream.close()
+
+            socket.close()
+
+        } catch {
+            case e: Exception =>
+                log.error("Error:" + e)
+        }
 
         responseMsg
     }
