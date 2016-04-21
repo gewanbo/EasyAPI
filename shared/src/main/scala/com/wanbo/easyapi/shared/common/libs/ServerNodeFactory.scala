d2ecc72af4da0f69eb17f18f5a24bfedc20d5105
wanbo <gewanbo@gmail.com>
2016-04-21 14:23:12 +0800

    Define server node with host and port.

---
+++ shared/src/main/scala/com/wanbo/easyapi/shared/common/libs/ServerNodeFactory.scala
@@ -0,0 +1,21 @@
+package com.wanbo.easyapi.shared.common.libs
+
+/**
+  * Server node factory
+  * Created by wanbo on 16/4/21.
+  */
+object ServerNodeFactory {
+
+    def parse(serverStr: String): ServerNode ={
+        val hostAndPort = serverStr.split(":")
+
+        if(hostAndPort.length == 2)
+            ServerNode(hostAndPort(0), hostAndPort(1).toInt)
+        else
+            null
+    }
+
+}
+
+
+case class ServerNode(host: String, port: Int)
