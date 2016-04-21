f50449624d9000a5da031bde64ae16fe8fdf5ad8
wanbo <gewanbo@gmail.com>
2016-04-21 15:38:16 +0800

    Add a simple HTTP request method.

--- shared/src/main/scala/com/wanbo/easyapi/shared/common/utils/Utils.scala
+++ shared/src/main/scala/com/wanbo/easyapi/shared/common/utils/Utils.scala
@@ -1,14 +1,19 @@
 package com.wanbo.easyapi.shared.common.utils
 
+import java.io._
 import java.math.BigInteger
+import java.net.Socket
 import java.text.SimpleDateFormat
-import java.util.{TimeZone, Calendar}
+import java.util.{Calendar, TimeZone}
+
+import com.wanbo.easyapi.shared.common.Logging
+import com.wanbo.easyapi.shared.common.libs.ServerNode
 
 /**
  * Utility functions.
  * Created by wanbo on 15/9/11.
  */
-object Utils {
+object Utils extends Logging {
 
     def MD5(str: String): String ={
         val m = java.security.MessageDigest.getInstance("MD5")
@@ -40,5 +45,48 @@ object Utils {
         }
 
         tabSet
+    }
+
+    /**
+      * Make a simple request
+      *
+      * @param server    The server host and port
+      * @param msg       The message will be send
+      * @param timeOut   Request timeout
+      * @return          The response string
+      */
+    def simpleRequest(server: ServerNode, msg: String, timeOut: Int = 30000): String ={
+        var responseMsg = ""
+
+        try {
+
+            val socket = new Socket(server.host, server.port)
+            socket.setSoTimeout(timeOut)
+
+            val outStream = socket.getOutputStream
+
+            val out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outStream)))
+
+            val inStream = new InputStreamReader(socket.getInputStream)
+            val in = new BufferedReader(inStream)
+
+            out.println(msg)
+            out.flush()
+
+            responseMsg = in.readLine()
+
+            out.close()
+            outStream.close()
+
+            in.close()
+            inStream.close()
+
+            socket.close()
+        } catch {
+            case e: Exception =>
+                log.error("Error:", e)
+        }
+
+        responseMsg
     }
 }
