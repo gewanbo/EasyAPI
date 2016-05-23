d0fb91a6ee2755e99ecc796d0f93b9035a4ceb91
wanbo <gewanbo@gmail.com>
2016-05-23 14:13:10 +0800

    Add configuration variable.

--- server/src/main/scala/com/wanbo/easyapi/server/database/DbDriver.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/database/DbDriver.scala
@@ -1,8 +1,11 @@
 package com.wanbo.easyapi.server.database
 
+import com.wanbo.easyapi.shared.common.libs.EasyConfig
+
 /**
   * Created by wanbo on 16/5/23.
   */
 abstract class DbDriver {
+    protected var _conf: EasyConfig = null
     protected def close()
 }
