b1b84826fd7d24c1e928d0f73e859ce33b6c002d
wanbo <gewanbo@gmail.com>
2016-06-15 15:54:35 +0800

    Setting app version.

--- server/src/main/bin/startup.sh
+++ server/src/main/bin/startup.sh
@@ -10,12 +10,12 @@ case "`uname`" in
 		;;
 esac
 base=${bin_abs_path}/..
-easy_conf=$base/conf/config.properties
-logback_configurationFile=$base/conf/logback.xml
+easy_conf=${base}/conf/config.properties
+logback_configurationFile=${base}/conf/logback.xml
 export LANG=en_US.UTF-8
-export BASE=$base
+export BASE=${base}
 
-if [ -f $base/bin/easyapi.pid ] ; then
+if [ -f ${base}/bin/easyapi.pid ] ; then
 	echo "found easyapi.pid , Please run stop.sh first ,then startup.sh" 2>&2
     exit 1
 fi
@@ -28,10 +28,10 @@ fi
 DEV_JAVA="/usr/bin/java"
 PRODUCT_JAVA="/local/server/java/bin/java"
 if [ -z "$JAVA" ]; then
-  if [ -f $DEV_JAVA ] ; then
-  	JAVA=$DEV_JAVA
-  elif [ -f $PRODUCT_JAVA ] ; then
-  	JAVA=$PRODUCT_JAVA
+  if [ -f ${DEV_JAVA} ] ; then
+  	JAVA=${DEV_JAVA}
+  elif [ -f ${PRODUCT_JAVA} ] ; then
+  	JAVA=${PRODUCT_JAVA}
   else
   	echo "Cannot find a Java JDK. Please set either set JAVA or put java (>=1.5) in your PATH." 2>&2
     exit 1
@@ -44,16 +44,16 @@ in
 	;;
 1 )	
 	var=$*
-	if [ -f $var ] ; then 
-		easy_conf=$var
+	if [ -f ${var} ] ; then
+		easy_conf=${var}
 	else
 		echo "THE PARAMETER IS NOT CORRECT.PLEASE CHECK AGAIN."
         exit
 	fi;;
 2 )	
 	var=$1
-	if [ -f $var ] ; then
-		easy_conf=$var
+	if [ -f ${var} ] ; then
+		easy_conf=${var}
 	else 
 		if [ "$1" = "debug" ]; then
 			DEBUG_PORT=$2
@@ -66,35 +66,40 @@ in
 	exit;;
 esac
 
-str=`file -L $JAVA | grep 64-bit`
+str=`file -L ${JAVA} | grep 64-bit`
 if [ -n "$str" ]; then
 	JAVA_OPTS="-server -Xms2048m -Xmx3072m -Xmn1024m -XX:SurvivorRatio=2 -XX:PermSize=96m -XX:MaxPermSize=256m -Xss256k -XX:-UseAdaptiveSizePolicy -XX:MaxTenuringThreshold=15 -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:+HeapDumpOnOutOfMemoryError"
 else
 	JAVA_OPTS="-server -Xms1024m -Xmx1024m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:MaxPermSize=128m "
 fi
 
-JAVA_OPTS=" $JAVA_OPTS -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8"
-EASY_OPTS="-DappName=easyapi -Dlogback.configurationFile=$logback_configurationFile -Deasy.conf=$easy_conf"
+### Set app version
+libFile=`ls -l ${base}/lib/easyapi.server*|awk '{print $9}'`
+libFile1=${libFile#*-}
+appVersion=${libFile1%-*}
 
-if [ -e $easy_conf -a -e $logback_configurationFile ]
+JAVA_OPTS=" $JAVA_OPTS -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8"
+EASY_OPTS="-DappName=easyapi -DappVersion=$appVersion -Dlogback.configurationFile=$logback_configurationFile -Deasy.conf=$easy_conf"
+
+if [ -e ${easy_conf} -a -e ${logback_configurationFile} ]
 then 
 	
-	for i in $base/lib/*;
-		do CLASSPATH=$i:"$CLASSPATH";
+	for i in ${base}/lib/*;
+		do CLASSPATH=${i}:"$CLASSPATH";
 	done
  	CLASSPATH="$base/conf:$CLASSPATH";
  	
  	echo "cd to $bin_abs_path for workaround relative path"
-  	cd $bin_abs_path
+  	cd ${bin_abs_path}
  	
-	echo LOG CONFIGURATION : $logback_configurationFile
-	echo Easy conf : $easy_conf
-	echo CLASSPATH :$CLASSPATH
-	$JAVA $JAVA_OPTS $JAVA_DEBUG_OPT $EASY_OPTS -classpath .:$CLASSPATH com.wanbo.easyapi.server.EasyServer 1>>$base/logs/out.log 2>&1 &
-	echo $! > $base/bin/easyapi.pid
+	echo LOG CONFIGURATION : ${logback_configurationFile}
+	echo Easy conf : ${easy_conf}
+	echo CLASSPATH :${CLASSPATH}
+	${JAVA} ${JAVA_OPTS} ${JAVA_DEBUG_OPT} ${EASY_OPTS} -classpath .:${CLASSPATH} com.wanbo.easyapi.server.EasyServer 1>>${base}/logs/out.log 2>&1 &
+	echo $! > ${base}/bin/easyapi.pid
 	
 	echo "cd to $current_path for continue"
-  	cd $current_path
+  	cd ${current_path}
 else 
-	echo "Easy conf("$easy_conf") OR log configration file($logback_configurationFile) is not exist,please create then first!"
+	echo "Easy conf("${easy_conf}") OR log configuration file($logback_configurationFile) is not exist,please create then first!"
 fi
