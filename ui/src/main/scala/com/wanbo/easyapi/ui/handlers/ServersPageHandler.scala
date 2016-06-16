f55e69af3c5099f862f4ceef82e1076ed542cedf
wanbo <gewanbo@gmail.com>
2016-06-16 11:52:12 +0800

    Complete setting information display.

--- ui/src/main/scala/com/wanbo/easyapi/ui/handlers/ServersPageHandler.scala
+++ ui/src/main/scala/com/wanbo/easyapi/ui/handlers/ServersPageHandler.scala
@@ -2,10 +2,11 @@ package com.wanbo.easyapi.ui.handlers
 
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 
+import com.alibaba.fastjson.JSON
 import com.wanbo.easyapi.shared.common.Logging
 import com.wanbo.easyapi.shared.common.libs.{EasyConfig, ServerNodeFactory}
 import com.wanbo.easyapi.shared.common.utils.ZookeeperClient
-import com.wanbo.easyapi.ui.lib.UIUtils
+import com.wanbo.easyapi.ui.lib.{ServerSetting, UIUtils}
 import com.wanbo.easyapi.ui.pages.WebPage
 import org.eclipse.jetty.server.Request
 import org.eclipse.jetty.server.handler.{AbstractHandler, ContextHandler}
@@ -18,6 +19,10 @@ import scala.xml.Node
  */
 class ServersPageHandler(conf: EasyConfig, contextPath: String, page: WebPage) extends ContextHandler with Logging {
 
+    private var serverPortHits = Seq[(String, Long)]()
+    private var serverSet = Set[String]()
+    private var serverSettings = Set[ServerSetting]()
+
     val handler = new AbstractHandler {
         override def handle(s: String, request: Request, httpServletRequest: HttpServletRequest, httpServletResponse: HttpServletResponse): Unit = {
             httpServletResponse.setContentType("text/html; charset=utf-8")
@@ -25,8 +30,10 @@ class ServersPageHandler(conf: EasyConfig, contextPath: String, page: WebPage) e
 
             val out = httpServletResponse.getWriter
 
+            collectData()
+
             page.title = "Servers"
-            page.content = makeTable(availableServers)
+            page.content = makeTable()
 
             log.info("Response contents ------------ server")
             out.println(UIUtils.commonNavigationPage(page))
@@ -39,24 +46,23 @@ class ServersPageHandler(conf: EasyConfig, contextPath: String, page: WebPage) e
     this.setContextPath(contextPath)
     this.setHandler(handler)
 
-    private def availableServers: Seq[(String, Long)] = {
+    private def collectData(): Unit = {
 
-        var serverList = Seq[(String, Long)]()
         val zk = new ZookeeperClient(conf.zkHosts)
 
         val serverNode = "/easyapi/servers"
 
         val servers = zk.getChildren(serverNode)
 
-        servers.foreach(s => {
+        serverPortHits = servers.map(serverWithPort => {
             var hitNum = 0L
-            val hitData = zk.get(serverNode + "/" + s)
+            val hitData = zk.get(serverNode + "/" + serverWithPort)
 
             if (hitData != null) {
                 try {
                     val hits = new String(hitData)
 
-                    log.info("Server [%s] - hits [%s] --------".format(s, hits))
+                    log.info("Server [%s] - hits [%s] --------".format(serverWithPort, hits))
 
                     hitNum = hits.toLong
                 } catch {
@@ -64,16 +70,35 @@ class ServersPageHandler(conf: EasyConfig, contextPath: String, page: WebPage) e
                 }
             }
 
-            serverList = serverList :+(s, hitNum)
+            (serverWithPort, hitNum)
+        })
+
+        serverSet = servers.map(server => {
+            val serverNode = ServerNodeFactory.parse(server)
+            serverNode.host
+        }).toSet
+
+        serverSet.foreach(server => {
+            val serverSettingRoot = "/easyapi/settings/servers/" + server
+            if(zk.exists(serverSettingRoot)){
+                val serverSettingBytes = zk.get(serverSettingRoot)
+                if(serverSettingBytes != null){
+                    val serverSettingData = new String(serverSettingBytes)
+                    val settingObj = JSON.parseObject(serverSettingData)
+                    val version = settingObj.getString("Version")
+                    val host = settingObj.getString("Host")
+                    val startTime = settingObj.getString("StartTime")
+                    serverSettings = serverSettings + ServerSetting(version, host, startTime)
+                }
+            }
         })
 
         zk.close()
-        serverList
     }
 
-    private def makeTable(data: Seq[(String, Long)]): Seq[Node] = {
+    private def makeTable(): Seq[Node] = {
 
-        val servers = data.map(x => {
+        val servers = serverPortHits.map(x => {
             val serverNode = ServerNodeFactory.parse(x._1)
             (serverNode.host, serverNode.port)
         }).groupBy(_._1).map(x => {
@@ -81,15 +106,25 @@ class ServersPageHandler(conf: EasyConfig, contextPath: String, page: WebPage) e
             val server = x._1
             val ports = x._2.map(_._2).mkString(",")
 
+            var version = "0.0"
+            var startTime = "0"
+
+            val setting = serverSettings.find(_.host == server)
+            setting.foreach(s => {
+                version = s.version
+                startTime = s.startTime
+            })
+
             <tr>
                 <td>{server}</td>
                 <td>{ports}</td>
-                <td>1.1.4</td>
-                <td>Running</td>
+                <td>{version}</td>
+                <td>{startTime}</td>
+                <td>0</td>
             </tr>
         })
 
-        val rows = data.map(r => {
+        val rows = serverPortHits.map(r => {
             <tr>
                 <td>{r._1}</td>
                 <td>{r._2}</td>
@@ -104,7 +139,8 @@ class ServersPageHandler(conf: EasyConfig, contextPath: String, page: WebPage) e
                         <th>Server</th>
                         <th>Ports</th>
                         <th>Version</th>
-                        <th>Status</th>
+                        <th>Start Time</th>
+                        <th>Requests Per Second</th>
                     </tr>
                 </thead>
                 <tbody>
---
+++ ui/src/main/scala/com/wanbo/easyapi/ui/lib/ServerSetting.scala
@@ -0,0 +1,6 @@
+package com.wanbo.easyapi.ui.lib
+
+/**
+  * Created by wanbo on 16/6/16.
+  */
+case class ServerSetting(version: String = "", host: String = "", startTime: String = "")
