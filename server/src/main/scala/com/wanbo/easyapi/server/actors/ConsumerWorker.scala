74681b18fa7aff7c4d6e7dffa70958cf66318072
wanbo <gewanbo@gmail.com>
2016-06-27 17:55:23 +0800

    Add log message.

--- server/src/main/scala/com/wanbo/easyapi/server/actors/ConsumerWorker.scala
+++ server/src/main/scala/com/wanbo/easyapi/server/actors/ConsumerWorker.scala
@@ -5,6 +5,7 @@ import com.wanbo.easyapi.server.lib.SeederManager
 import com.wanbo.easyapi.server.messages.CacheUpdate
 import com.wanbo.easyapi.shared.common.Logging
 import com.wanbo.easyapi.shared.common.libs.EasyConfig
+import org.slf4j.MDC
 
 /**
  * The worker for consuming message.
@@ -20,6 +21,10 @@ class ConsumerWorker(conf: EasyConfig) extends Actor with Logging {
             val seederManager = new SeederManager(conf, "")
             val ret = seederManager.updateCache(seeder, seed)
 
+            MDC.put("destination", "cache")
+            log.info(ret.oelement.toString())
+            MDC.clear()
+            
         case _ =>
             log.info("Unknown message.!!!")
     }
