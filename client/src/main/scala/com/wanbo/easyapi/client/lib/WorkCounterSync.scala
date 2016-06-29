a0608c0c93e17d284d8de04e2f58a41ae1f7f59f
wanbo <gewanbo@gmail.com>
2016-06-29 14:23:12 +0800

    Switch JSON library to FASTJSON.

--- client/src/main/scala/com/wanbo/easyapi/client/lib/WorkCounterSync.scala
+++ client/src/main/scala/com/wanbo/easyapi/client/lib/WorkCounterSync.scala
@@ -1,10 +1,9 @@
 package com.wanbo.easyapi.client.lib
 
+import com.alibaba.fastjson.JSONObject
 import com.wanbo.easyapi.shared.common.Logging
 import com.wanbo.easyapi.shared.common.libs.{EasyConfig, ZookeeperManager}
 import com.wanbo.easyapi.shared.common.utils.ZookeeperClient
-
-import scala.util.parsing.json.JSONObject
 
 /**
  * Sync work count data.
@@ -15,22 +14,21 @@ object WorkCounterSync extends ZookeeperManager with Logging {
     def sync(conf: EasyConfig): Unit ={
         try {
 
-            var missData = Map[String, Any]()
+            val missData = new JSONObject()
             WorkCounter.getSummary.foreach(server => {
-                missData += server._1 -> server._2
+                missData.put(server._1, server._2)
             })
 
-            val data = Map[String, Any]("miss" -> JSONObject(missData))
+            val jsonData = new JSONObject()
+            jsonData.put("miss", missData)
 
-            val jsonData = JSONObject(data)
-
-            log.info("-------sync data:" + jsonData.toString())
+            log.info("-------sync data:" + jsonData.toJSONString)
 
             val zk = new ZookeeperClient(conf.zkHosts, 3000, app_root, Some(this.callback))
 
             val clientNode = client_root + "/" + conf.clientId
             if(zk.exists(clientNode)){
-                zk.set(clientNode, jsonData.toString().map(_.toByte).toArray)
+                zk.set(clientNode, jsonData.toJSONString.map(_.toByte).toArray)
             }
 
             zk.close()
