f7dd39267922c129e68d25897d74914d098177c5
wanbo <gewanbo@gmail.com>
2016-07-06 15:39:53 +0800

    Add mongo driver.

---
+++ server/src/main/scala/com/wanbo/easyapi/server/database/MongoDriver.scala
@@ -0,0 +1,57 @@
+package com.wanbo.easyapi.server.database
+
+import com.wanbo.easyapi.shared.common.libs.EasyConfig
+import org.mongodb.scala.connection.ClusterSettings
+import org.mongodb.scala.{MongoClient, MongoClientSettings, MongoCollection, Document, ServerAddress}
+import org.slf4j.LoggerFactory
+
+import scala.collection.JavaConverters._
+
+/**
+ * MongoDB driver.
+ * Created by wanbo on 16/7/5.
+ */
+case class MongoDriver() extends DbDriver with IDriver {
+    private var _client: MongoClient = null
+    private var _coll: MongoCollection[Document] = null
+
+    private val log = LoggerFactory.getLogger(classOf[MongoDriver])
+
+    override def setConfiguration(conf: EasyConfig): Unit = {
+        this._conf = conf
+
+        val settings = conf.driverSettings.filter(x => x._2.get("type").get == "mongo").toList.map(_._2)
+
+        if(settings.nonEmpty) {
+            val serverAddress = ServerAddress(settings.head.getOrElse("host", ""), settings.head.getOrElse("port", "0").toInt)
+            val clusterSetting = ClusterSettings.builder().hosts(List(serverAddress).asJava).build()
+            val clientSettings = MongoClientSettings.builder().clusterSettings(clusterSetting).build()
+
+            _client = MongoClient(clientSettings)
+        }
+
+    }
+
+    def getCollection(dbName: String, collection: String): MongoCollection[Document] ={
+
+        try {
+            val db = _client.getDatabase(dbName)
+            _coll = db.getCollection(collection)
+
+        } catch {
+            case e: Exception =>
+                log.error("Error:", e)
+        }
+
+        _coll
+    }
+
+    protected def close(): Unit ={
+        try{
+            if(_client != null)
+                _client.close()
+        } catch {
+            case e: Exception =>
+        }
+    }
+}
