b5bea996c3fca358e22e957e0d0e25a0ec792859
wanbo <gewanbo@gmail.com>
2016-07-15 14:38:02 +0800

    Add worker status enumeration.

---
+++ server/src/main/scala/com/wanbo/easyapi/server/lib/WorkerStatus.scala
@@ -0,0 +1,13 @@
+package com.wanbo.easyapi.server.lib
+
+/**
+  * Created by wanbo on 16/7/15.
+  * @author wanbo<gewanbo@gmail.com>
+  */
+object WorkerStatus {
+
+    val NOT_START = 1
+    val STARTED = 2
+    val STOPPED = 3
+
+}
